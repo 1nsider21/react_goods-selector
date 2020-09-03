533a61e3426e1c522896ca22c2a499b4b0218e82
Artur <artroyal.pro@gmail.com>
2020-09-03 17:55:09 +0300

    Updates

---
+++ precache-manifest.b0688d2f301a88c9a52a1ad140f7a96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea27b16e025125568ce60225d650de5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b71818006da8ecdf71cb",
+    "url": "./static/css/main.ede068e0.chunk.css"
+  },
+  {
+    "revision": "4dd0f18127de32a9553a",
+    "url": "./static/js/2.22b5d565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.22b5d565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71818006da8ecdf71cb",
+    "url": "./static/js/main.f66680b3.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
